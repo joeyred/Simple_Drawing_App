3e780294b06e16e7d9f2ecaee1b8339369a05573
Joey Hayes <bjoeyhayes@gmail.com>
2016-03-15 21:24:50 -0400

    lines can be drawn on canvas

--- js/app.js
+++ js/app.js
@@ -5,6 +5,12 @@
 
 // Store current selected color on load
 var color = $('.selected').css('background-color');
+var $canvas = $('canvas');
+// $('canvas')[0] is the same as document.getElementByTagName('canvas')[0]
+var context = $canvas[0].getContext('2d');
+var lastEvent;
+var mouseDown = false;
+
 
 // when clicking color control list items
 $( '.controls' ).on('click', 'li', function() {
@@ -55,4 +61,33 @@ $('#addNewColor').click(function() {
 	// select the new color
 
 // On mouse events on the canvas
-	// Draw lines
+$canvas.mousedown(function(e){
+
+	lastEvent = e;
+	mouseDown = true;
+}).mousemove(function(e){
+	if (mouseDown) {
+		context.beginPath();
+		context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+		context.lineTo(e.offsetX, e.offsetY);
+		context.strokeStyle = color;
+		context.stroke();
+		lastEvent = e;
+	}
+}).mouseup(function(){
+	mouseDown = false;
+}).mouseleave(function(){
+	$canvas.mouseup();
+});
+
+
+
+
+
+
+
+
+
+
+
+
